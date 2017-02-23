12d36507c5201fd326de31545b848369c661c815
Mingi Kim <ksmk99@gmail.com>
2017-02-23 10:12:12 -0600

    Fix bugs

--- src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -16,10 +16,10 @@ public class Router extends Device
 {	
 	/** Routing table for the router */
 	private RouteTable routeTable;
-	
+
 	/** ARP cache for the router */
 	private ArpCache arpCache;
-	
+
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
@@ -30,13 +30,13 @@ public class Router extends Device
 		this.routeTable = new RouteTable();
 		this.arpCache = new ArpCache();
 	}
-	
+
 	/**
 	 * @return routing table for the router
 	 */
 	public RouteTable getRouteTable()
 	{ return this.routeTable; }
-	
+
 	/**
 	 * Load a new routing table from a file.
 	 * @param routeTableFile the name of the file containing the routing table
@@ -49,13 +49,13 @@ public class Router extends Device
 					+ routeTableFile);
 			System.exit(1);
 		}
-		
+
 		System.out.println("Loaded static route table");
 		System.out.println("-------------------------------------------------");
 		System.out.print(this.routeTable.toString());
 		System.out.println("-------------------------------------------------");
 	}
-	
+
 	/**
 	 * Load a new ARP cache from a file.
 	 * @param arpCacheFile the name of the file containing the ARP cache
@@ -68,7 +68,7 @@ public class Router extends Device
 					+ arpCacheFile);
 			System.exit(1);
 		}
-		
+
 		System.out.println("Loaded static ARP cache");
 		System.out.println("----------------------------------");
 		System.out.print(this.arpCache.toString());
@@ -83,8 +83,8 @@ public class Router extends Device
 	public void handlePacket(Ethernet etherPacket, Iface inIface)
 	{
 		System.out.println("*** -> Received packet: " +
-                etherPacket.toString().replace("\n", "\n\t"));
-		
+				etherPacket.toString().replace("\n", "\n\t"));
+
 		/********************************************************************/
 		/* TODO: Handle packets                                             */
 
@@ -95,80 +95,78 @@ public class Router extends Device
 			System.out.println("Packet Type is not IPv4");
 			return;
 		}
-		
+
 		IPv4 packet = (IPv4) etherPacket.getPayload();
-		
-		System.out.println("version :" + packet.getVersion() + ", source addr :" +packet.getSourceAddress() + ", dest addr :"+ packet.getDestinationAddress());
-		
+
 		// 2. Check the packet checksum
 		short checksum = packet.getChecksum();
 
 		byte[] data = packet.serialize();
-        ByteBuffer bb = ByteBuffer.wrap(data);
-        
-        bb.putShort(10,(short) 0x0000);
+		ByteBuffer bb = ByteBuffer.wrap(data);
 
-        // calculate checksum
-        int accumulation = 0;
-        for (int i = 0; i < packet.getHeaderLength() * 2; ++i) {
-            accumulation += 0xffff & bb.getShort();
-        }
-        accumulation = ((accumulation >> 16) & 0xffff)
-                + (accumulation & 0xffff);
-        
-        short checksumCalcd = (short)(~accumulation & 0xffff);
-        
-        if(checksum != checksumCalcd){
-        	// if calculated checksum is different from received one
-        	// drop packet
+		bb.putShort(10,(short) 0x0000);
+
+		// calculate checksum
+		int accumulation = 0;
+		for (int i = 0; i < packet.getHeaderLength() * 2; ++i) {
+			accumulation += 0xffff & bb.getShort();
+		}
+		accumulation = ((accumulation >> 16) & 0xffff)
+				+ (accumulation & 0xffff);
+
+		short checksumCalcd = (short)(~accumulation & 0xffff);
+
+		if(checksum != checksumCalcd){
+			// if calculated checksum is different from received one
+			// drop packet
 			System.out.println("Checksum is not matched (ori:"+checksum+", calcd:"+checksumCalcd+")");
-        	return;
-        }
-        
-        // 3. Decrease and Check TTL
-        byte ttl = packet.getTtl();
-        ttl--;
-        
-        if(ttl==0){
-        	// if ttl becomes 0
-        	// drop packet
+			return;
+		}
+
+		// 3. Decrease and Check TTL
+		byte ttl = packet.getTtl();
+		ttl--;
+
+		if(ttl==0){
+			// if ttl becomes 0
+			// drop packet
 			System.out.println("TTL expired");
-        	return;
-        }
-        packet.setTtl(ttl);
-        
-        // 4. Check interfaces that has dest IP.
-        for(Entry<String, Iface> entry : this.interfaces.entrySet()){
-        	Iface iface = entry.getValue();
-        	if(iface.getIpAddress() == packet.getDestinationAddress()){
-        		// if router has interface for dest ip
-        		// drop packet
-    			System.out.println("destination interface found");
-        		return;
-        	}
-        }
-        
-        // 5. lookup route table
-        RouteEntry routeEntry = this.getRouteTable().lookup(packet.getDestinationAddress());
-        if(routeEntry == null){
-        	// if no route entry matches
-        	// drop packet
+			return;
+		}
+		packet.setTtl(ttl);
+
+		// 4. Check interfaces that has dest IP.
+		for(Entry<String, Iface> entry : this.interfaces.entrySet()){
+			Iface iface = entry.getValue();
+			if(iface.getIpAddress() == packet.getDestinationAddress()){
+				// if router has interface for dest ip
+				// drop packet
+				System.out.println("destination interface found");
+				return;
+			}
+		}
+
+		// 5. lookup route table
+		RouteEntry routeEntry = this.getRouteTable().lookup(packet.getDestinationAddress());
+		if(routeEntry == null){
+			// if no route entry matches
+			// drop packet
 			System.out.println("Destination interface found");
-        	return;
-        }
-        
-        // 6. lookup ARP cache and change the packet's dest mac addr
-        ArpEntry arpEntry = this.arpCache.lookup(packet.getDestinationAddress());
-        etherPacket.setDestinationMACAddress(arpEntry.getMac().toBytes());
-        etherPacket.setSourceMACAddress(routeEntry.getInterface().getMacAddress().toBytes());
-        
-        // 7. update checksum
+			return;
+		}
+
+		// 6. lookup ARP cache and change the packet's dest mac addr
+		ArpEntry arpEntry = this.arpCache.lookup(packet.getDestinationAddress());
+		etherPacket.setDestinationMACAddress(arpEntry.getMac().toBytes());
+		etherPacket.setSourceMACAddress(routeEntry.getInterface().getMacAddress().toBytes());
+
+		// 7. update checksum
 		packet.resetChecksum();
-        packet.serialize();
-		
-        // 8. send packet
-        sendPacket(etherPacket, routeEntry.getInterface());
-		
+		packet.serialize();
+
+		// 8. send packet
+		sendPacket(etherPacket, routeEntry.getInterface());
+
 		/********************************************************************/
 	}
 }
